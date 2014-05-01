7679b9cf3798049ced48606f0ec89f32f21c5806
Zac Connelly <zconnelly13@gmail.com>
2014-05-01 17:26:25 +0000

    get a working version on the local machine, add to gitignore

--- public_files/js/phone.js
+++ public_files/js/phone.js
@@ -1,1 +1,10 @@
-alert("success!");
+function deviceMotionHandler(event) {
+    var rotation = event.rotationRate;
+    var alpha = Math.round(rotation.alpha*10);
+    var beta  = Math.round(rotation.beta*10);
+    var gamma = Math.round(rotation.gamma*10);
+    var data = 'Alpha: ' + alpha + '<br />Beta: ' + beta + '<br />Gamma: ' + gamma;
+    document.getElementById('output').innerHTML = data
+}
+
+window.addEventListener('devicemotion',deviceMotionHandler,false);
