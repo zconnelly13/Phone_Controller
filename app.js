f84c47b374b368278578eac732870396eeb623b2
Zac Connelly <zconnelly13@gmail.com>
2014-05-01 17:49:03 +0000

    install primus dependencies

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// Serve Static Files
 var express = require('express');
 var app = express();
-
 var oneDay = 86400000;
 app.use(express.static(__dirname + '/public_files', { maxAge: oneDay}));
 app.listen(process.env.PORT || 1313);
+
+// Primus
+'use strict';
+var Primus = require('primus')
+    , http = require('http')
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("Primus!?!?!?");
+}), primus = new Primus(server,{});
+
+server.listen(1310);
