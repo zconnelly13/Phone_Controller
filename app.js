8ed975a75843743454ea3475dd97bd437629a500
Zac Connelly <zconnelly13@gmail.com>
2014-05-01 16:53:33 +0000

    serve static content with node

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/public_files', { maxAge: oneDay}));
+app.listen(process.env.PORT || 1313);
