dfbcbe55bbfbf67b5f06bb487cce0259893fea00
Zac Connelly <zconnelly13@gmail.com>
2014-05-01 17:35:15 +0000

    local rotation data done :)

--- public_files/js/phone.js
+++ public_files/js/phone.js
@@ -7,4 +7,13 @@ function deviceMotionHandler(event) {
     document.getElementById('output').innerHTML = data
 }
 
+function deviceOrientationHandler(event) {
+    var alpha = Math.round(event.alpha*10);
+    var beta  = Math.round(event.beta*10);
+    var gamma = Math.round(event.gamma*10);
+    var data = 'Alpha: ' + alpha + '<br />Beta: ' + beta + '<br />Gamma: ' + gamma;
+    document.getElementById('currentOrientation').innerHTML = data
+}
+
 window.addEventListener('devicemotion',deviceMotionHandler,false);
+window.addEventListener('deviceorientation',deviceOrientationHandler,false);
